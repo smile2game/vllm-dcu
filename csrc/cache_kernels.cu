dd823f7fc909ea036ee65e56f17fbf25626ef460
zhuwenwen <1536492181@qq.com>
2024-03-25 14:32:48 +0800

    add bf16

--- csrc/cache_kernels.cu
+++ csrc/cache_kernels.cu
@@ -251,8 +251,8 @@ void reshape_and_cache(
       CALL_RESHAPE_AND_CACHE(float, float, false);
     } else if (key.dtype() == at::ScalarType::Half) {
       CALL_RESHAPE_AND_CACHE(uint16_t, uint16_t, false);
-    // } else if (key.dtype() == at::ScalarType::BFloat16) {
-    //   CALL_RESHAPE_AND_CACHE(__nv_bfloat16, __nv_bfloat16, false);
+    } else if (key.dtype() == at::ScalarType::BFloat16) {
+      CALL_RESHAPE_AND_CACHE(__nv_bfloat16, __nv_bfloat16, false);
     }
   // } else if (kv_cache_dtype == "fp8_e5m2") {
   //   if (key.dtype() == at::ScalarType::Float) {
@@ -308,8 +308,8 @@ void convert_fp8_e5m2(
     CALL_CONVERT_FP8_E5M2(uint8_t, float);
   } else if (src_cache.dtype() == at::ScalarType::Half) {
     CALL_CONVERT_FP8_E5M2(uint8_t, uint16_t);
-  // } else if (src_cache.dtype() == at::ScalarType::BFloat16) {
-  //   CALL_CONVERT_FP8_E5M2(uint8_t, __nv_bfloat16);
+  } else if (src_cache.dtype() == at::ScalarType::BFloat16) {
+    CALL_CONVERT_FP8_E5M2(uint8_t, __nv_bfloat16);
   } else if (dst_cache.dtype() == at::ScalarType::Float) {
     CALL_CONVERT_FP8_E5M2(float, uint8_t);
   } else if (dst_cache.dtype() == at::ScalarType::Half) {
--- csrc/dispatch_utils.h
+++ csrc/dispatch_utils.h
@@ -8,8 +8,8 @@
 
 #define VLLM_DISPATCH_CASE_FLOATING_TYPES(...)              \
   AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)      \
-  AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)       
-  // AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)
+  AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)       \
+  AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)
 
 #define VLLM_DISPATCH_FLOATING_TYPES(TYPE, NAME, ...)             \
   AT_DISPATCH_SWITCH(                                             \
@@ -17,8 +17,8 @@
 
 #define VLLM_DISPATCH_CASE_FLOATING_AND_BYTE_TYPES(...)     \
   AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)      \
-  AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)       
-  // AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)   \
+  AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)       \
+  AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__)   
   // AT_DISPATCH_CASE(at::ScalarType::Byte, __VA_ARGS__)
 
 #define VLLM_DISPATCH_FLOATING_AND_BYTE_TYPES(TYPE, NAME, ...)           \
